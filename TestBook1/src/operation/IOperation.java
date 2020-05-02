617a84ebf52c3c2a8a39e9aecf7dbefe80604002
Qian-Congcong <2629180149@qq.com>
2020-05-02 23:13:59 +0800

    代码更新

---
+++ TestBook1/src/operation/IOperation.java
@@ -0,0 +1,15 @@
+package operation;
+
+import book.BookList;
+
+/**
+ * program: TestBook
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: YouName
+ * Date:2020-04-30 19
+ * Time:48
+ */
+public interface IOperation {
+    void work(BookList bookList);
+}
